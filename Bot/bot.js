8e9a2f13e96301e332c95faf05079f934de3ba4f
kuupertjuhh <133634292+kuupertjuhh@users.noreply.github.com>
2023-07-08 12:18:07 +0200

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "6166731294:AAE2nCPpgDVmqNUsdk47Mjg_HYrqFgRBX2g";
+const TOKEN = "";
 const bot = new Telegraf(TOKEN);
 
 const web_link = "https://jocular-chaja-b37635.netlify.app/";
